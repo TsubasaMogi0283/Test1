3a5bc5af164beb93827986123b18a96dcf0d4525
TM <m.tsubasa0512@gmail.com>
2026-06-08 15:53:13 +0900

    問4

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
 ﻿#include <stdio.h>
+#define CLASS_SIZE 5
+#define GRADE_SIZE 4
 
 int main() {
-	char str[10] = "";
+	const int kSchoolSize = 3;
 
-	//今回はKamataと入力します。
-	printf("文字列を入力してください\n");
-	//～～～～("%s", str);①
-	scanf_s("%s", str, 10);
+	//int data[①] = { 31,31,31,30,18 };
+	int data[CLASS_SIZE] = { 31,31,31,30,18 };
+	int data2[5] = { 1,2,3,4 };
 
-	//printf("%〇\n", str[1]);//②
-	printf("%c\n", str[1]);//③
+	printf("%d\n", data[0]);//②
+	data[4] = data[1];
+	printf("%d\n", data[4]);//③
+	//printf("%d\n", data[CLASS_SIZE]);//④配列の範囲外アクセスについてチェック
 
-	//printf("%〇\n", 〇);//④ c,s,d,t⑤&str,str
-	printf("%s\n", str);//⑥Kamata
+	int tmp = data[3];
+	data[1] = tmp + 5;
+	data[2] = data[1];
+	printf("%d", data[2]);//⑤
+	printf("%d", data2[4]);//⑥
 
 	return 0;
 }
