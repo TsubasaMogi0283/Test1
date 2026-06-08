d8f5da0903601fd69195e4f157dd92bcc4e66c7c
TM <m.tsubasa0512@gmail.com>
2026-06-08 15:50:14 +0900

    問2

--- main.cpp
+++ main.cpp
@@ -2,20 +2,17 @@
 
 int main() {
 
-	int numI = 7;
-	float numF = 8.0f;
-	double numD = 9.0;
+	int switchOne = 2017;
+	int wiiU = -2012;
+	int playStation5 = 2020;
+	float ds = 2004.0f;
+	float familyComputer = -1990.0f;
 
-	//printf("整数: %①, 浮動小数点: %②, 倍精度浮動小数点: %③\n", numI, numF, numD);
-	//7,8.000000,9.000000
-	printf("整数: %d, 浮動小数点: %f, 倍精度浮動小数点: %lf\n", numI, numF, numD);
-
-
-	//小数点と整数の関係
-	//8
-	//7.0
-	printf("%d\n", static_cast<int>(numF));//⑤
-	printf("%.1f\n", static_cast<float>(numI));//⑥
+	printf("(Switch:%06d)\n", switchOne);
+	printf("(Wii U:%06d)\n", wiiU);
+	printf("(PlayStation :%-06d)\n", playStation5);
+	printf("(DS:%010.3f)\n", ds);
+	printf("(FamilyComputer:%-10.3f)\n", familyComputer);
 
 	return 0;
 }
