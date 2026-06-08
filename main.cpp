31511e4a235ebd11b0c6848f5c5f75847a9a0c26
TM <m.tsubasa0512@gmail.com>
2026-06-08 15:49:49 +0900

    問1

--- main.cpp
+++ main.cpp
@@ -2,17 +2,20 @@
 
 int main() {
 
-    int score = 85; // テストするスコア
+	int numI = 7;
+	float numF = 8.0f;
+	double numD = 9.0;
 
-    printf("あなたの評価：");
+	//printf("整数: %①, 浮動小数点: %②, 倍精度浮動小数点: %③\n", numI, numF, numD);
+	//7,8.000000,9.000000
+	printf("整数: %d, 浮動小数点: %f, 倍精度浮動小数点: %lf\n", numI, numF, numD);
 
-    // 順序に注目！
-    if (score >= 60) {
-        printf("B判定（合格）\n");
-    } else if (score >= 80) {
-        printf("A判定（優秀！）\n");
-    } else {
-        printf("C判定（不合格）\n");
-    }
+
+	//小数点と整数の関係
+	//8
+	//7.0
+	printf("%d\n", static_cast<int>(numF));//⑤
+	printf("%.1f\n", static_cast<float>(numI));//⑥
+
 	return 0;
 }
