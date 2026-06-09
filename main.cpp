227d3a12cbf791e5187b7c766f00d080a6853cfe
TM <m.tsubasa0512@gmail.com>
2026-06-09 11:56:18 +0900

    名前変更。 ファミコンはnesらしいです。

--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@ int main() {
 
 	int switchOne = 2017;
 	int wiiU = -2012;
-	int playStation5 = 2020;
+	int ps5 = 2020;
 	float ds = 2004.0f;
-	float familyComputer = -1990.0f;
+	float nes = -1990.0f;
 
 	printf("(Switch:%06d)\n", switchOne);
 	printf("(Wii U:%06d)\n", wiiU);
-	printf("(PlayStation :%-06d)\n", playStation5);
+	printf("(PlayStation :%-06d)\n", ps5);
 	printf("(DS:%010.3f)\n", ds);
-	printf("(FamilyComputer:%-10.3f)\n", familyComputer);
+	printf("(ファミコン:%-10.3f)\n", nes);
 
 	return 0;
 }
